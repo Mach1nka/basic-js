64745b24085b649fc7815767031b3f7b33dfe9df
Artem <uspenskiyart@gmail.com>
2020-02-29 02:30:43 +0300

    some algoritms have been ready

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
+  arr:[],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position> this.arr.length || position - 1 < 0) {
+      this.arr = [];
+      throw new Error('error');
+    }else {
+      this.arr.splice(position-1 , 1);
+    }
+    return  this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     this.arr= this.arr.reverse();
+     return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     let res = this.arr.join('~~');
+     this.arr=[];
+     return res;
   }
 };
 
